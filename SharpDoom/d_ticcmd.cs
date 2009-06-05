835a33a2ed45b003f724a83769be92c9ef8ef443
jumpergrav <jumpergrav@a5f69ade-492a-11de-82d9-516ade3e46db>
2009-06-05 11:43:09 +0000

    * 99% Player class * stub for TicCmd * stub for Game

---
+++ SharpDoom/d_ticcmd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpDoom
+{
+    struct TicCmd
+    {
+        char forwardmove;	// *2048 for move
+        char sidemove;	// *2048 for move
+        short angleturn;	// <<16 for angle delta
+        short consistancy;	// checks for net game
+        byte chatchar;
+        byte buttons;
+    }
+}
